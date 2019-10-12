480885ad94335b788792118f99f638e7d69c67aa
johnywojtek <johnywojtek@gmail.com>
2019-10-12 14:54:58 +0200

    Updates

---
+++ precache-manifest.d3c4ff804a8e7750cd08db85e8c49251.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11af601995af31a5f3c88de03748c9ab",
+    "url": "/ERP-Frontend/index.html"
+  },
+  {
+    "revision": "4e37b5a5164508743c60",
+    "url": "/ERP-Frontend/static/css/2.e677a39d.chunk.css"
+  },
+  {
+    "revision": "4cc10c270eee518c86c4",
+    "url": "/ERP-Frontend/static/css/main.3fb1a876.chunk.css"
+  },
+  {
+    "revision": "4e37b5a5164508743c60",
+    "url": "/ERP-Frontend/static/js/2.7554ddc3.chunk.js"
+  },
+  {
+    "revision": "4cc10c270eee518c86c4",
+    "url": "/ERP-Frontend/static/js/main.b08c2dc4.chunk.js"
+  },
+  {
+    "revision": "36d112c1d634bb267634",
+    "url": "/ERP-Frontend/static/js/runtime~main.322e47e3.js"
+  },
+  {
+    "revision": "a3505ec72fcc8ad507f5d79bf70a15af",
+    "url": "/ERP-Frontend/static/media/neur.a3505ec7.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/ERP-Frontend/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/ERP-Frontend/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/ERP-Frontend/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/ERP-Frontend/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "70cede060d2df49e4a065acd4ca6b164",
+    "url": "/ERP-Frontend/static/media/user-group.70cede06.png"
+  }
+]);
