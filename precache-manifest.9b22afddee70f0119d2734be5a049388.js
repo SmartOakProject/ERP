824a66d285e7ba54333cec089e179bfccd2b4961
johnywojtek <johnywojtek@gmail.com>
2020-01-15 09:24:02 +0100

    Updates

---
+++ precache-manifest.9b22afddee70f0119d2734be5a049388.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df196a5f63e887f4ec8dfa264bb758f3",
+    "url": "/ERP-Frontend/index.html"
+  },
+  {
+    "revision": "3796802a1a06eb9b78d0",
+    "url": "/ERP-Frontend/static/css/2.d77089f1.chunk.css"
+  },
+  {
+    "revision": "227e68f9dca35ee9adb7",
+    "url": "/ERP-Frontend/static/css/main.4a48736f.chunk.css"
+  },
+  {
+    "revision": "3796802a1a06eb9b78d0",
+    "url": "/ERP-Frontend/static/js/2.d93a9d69.chunk.js"
+  },
+  {
+    "revision": "227e68f9dca35ee9adb7",
+    "url": "/ERP-Frontend/static/js/main.95f93aae.chunk.js"
+  },
+  {
+    "revision": "36d112c1d634bb267634",
+    "url": "/ERP-Frontend/static/js/runtime~main.322e47e3.js"
+  },
+  {
+    "revision": "a3505ec72fcc8ad507f5d79bf70a15af",
+    "url": "/ERP-Frontend/static/media/neur.a3505ec7.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/ERP-Frontend/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/ERP-Frontend/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/ERP-Frontend/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/ERP-Frontend/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "70cede060d2df49e4a065acd4ca6b164",
+    "url": "/ERP-Frontend/static/media/user-group.70cede06.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ErpReact/precache-manifest.f3527bafe5b0675cfe0094a412558a14.js"
+  "/ERP-Frontend/precache-manifest.9b22afddee70f0119d2734be5a049388.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ErpReact/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ERP-Frontend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
